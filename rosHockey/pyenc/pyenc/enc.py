5c37642e06b7d0cc8f3624e655febd0b3ec07159
Zack <zackwatkins604@gmail.com>
2021-10-26 12:26:06 -0700

    more ros stuff

--- rosHockey/pyenc/pyenc/enc.py
+++ rosHockey/pyenc/pyenc/enc.py
@@ -8,7 +8,7 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(MalletPos, 'topic', 10)
+        self.publisher_ = self.create_publisher(MalletPos, 'MalletPosition', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
