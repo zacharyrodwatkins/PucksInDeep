bf2ff12b944237fa416e77b8876a0f5018ebb38a
Ubuntu <ubuntu@ubuntu.mshome.net>
2021-11-19 01:43:24 +0000

    QT GUI

---
+++ RPi/qt_gui/main.py
@@ -0,0 +1,59 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QTimer
+import mplwidget
+import sys
+import BP_Coms
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        #Load the UI Page
+        uic.loadUi('layout.ui', self)
+        self.fin_x_vel.valueChanged.connect(self.update_fin_x_vel)
+        self.fin_y_vel.valueChanged.connect(self.update_fin_y_vel)
+        self.fin_x_acc.valueChanged.connect(self.update_fin_x_vel)
+        self.fin_y_acc.valueChanged.connect(self.update_fin_y_acc)
+        self.path_time.valueChanged.connect(self.update_path_time)
+        self.gen_button.clicked.connect(self.table_plot.canvas.generate_path)
+        self.send_button.clicked.connect(self.table_plot.canvas.send_path)
+
+        # Timer configuration
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_cur_mallet)
+        self.timer.start(10)
+
+    # Update doublespinbox value callbacks
+    def update_fin_x_vel(self, value):
+        self.table_plot.canvas.final_x_vel = value
+        print(self.table_plot.final_x_v)
+
+    def update_fin_y_vel(self, value):
+        self.table_plot.canvas.final_y_vel = value
+
+    def update_fin_x_acc(self, value):
+        self.table_plot.canvas.final_x_acc = value
+
+    def update_fin_y_acc(self, value):
+        self.table_plot.canvas.final_y_acc = value
+
+    def update_path_time(self, value):
+        self.table_plot.canvas.path_time = value
+
+    def update_cur_mallet(self):
+        stat = BP_Coms.get_mallet_stat()
+        if stat == -1:
+            return  # No mallet data available
+        if sum(stat[0:4]) == stat[4]:
+            (x,y) = stat[0:2]
+        self.table_plot.canvas.current_pos.set(xdata = x, ydata = y)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ RPi/qt_gui/mplwidget.py
@@ -0,0 +1,109 @@
+# Imports
+from PyQt5 import QtWidgets
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
+import matplotlib.pyplot as plt
+import matplotlib
+import BP_Coms
+import path_generator
+from PIL import Image
+
+# Ensure using PyQt5 backend
+matplotlib.use('QT5Agg')
+
+# Matplotlib canvas class to create figure
+class MplCanvas(Canvas):
+    def __init__(self, parent=None, **kwargs):        
+        # Making Matplotlib figure object with data
+        self.canvas = Canvas(Figure())
+        Canvas.__init__(self, self.canvas.figure)
+
+        # Configure base frame of interactive plot
+        playspace_width = 760
+        playspace_height = 1120
+        self.ax = self.canvas.figure.add_subplot(111)
+        self.ax.set_xlim((0, playspace_width))
+        self.ax.set_ylim((0, playspace_height))
+        self.half_table = Image.open('HalfTable.png')
+        self.half_table = self.half_table.resize((playspace_width, playspace_height), Image.ANTIALIAS).transpose(Image.FLIP_TOP_BOTTOM)
+        self.ax.imshow(self.half_table)
+
+        # Create Click/Drag callbacks
+        self.clicked = False
+        self.click_clb = self.canvas.mpl_connect('button_press_event', self.on_click)
+        self.release_clb = self.canvas.mpl_connect('button_release_event', self.on_release)
+        self.move_clb = self.canvas.mpl_connect('motion_notify_event', self.on_move)
+
+        # Configure layout
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.toolbar)
+        self.layout.addWidget(self.canvas)
+
+        # Setup path attributes
+        self.x = 0
+        self.y = 0
+        self.final_x_vel = 0
+        self.final_y_vel = 0
+        self.final_x_acc = 0
+        self.final_y_acc = 0
+        self.path_time = 1
+        self.x_path = [0]
+        self.y_path = [0]
+        self.x_stop = [0,0,0]
+        self.y_stop = [0,0,0]
+
+        # Setup plots
+        self.current_pos = self.ax.plot([self.x], [self.y], 'o', color='black', markersize=10)[0]
+        self.final_pos = self.ax.plot([self.x], [self.y], 'o', color='blue', markersize=10)[0]
+        self.path = self.ax.plot(self.x_path, self.y_path, color='red')[0]
+
+    def generate_path(self):
+        x_start = [self.current_pos.get_data()[0], 0, 0]
+        self.x_stop = [self.x, self.final_x_vel, self.final_x_acc]
+        y_start = [self.current_pos.get_data()[1], 0, 0]
+        self.y_stop = [self.y, self.final_y_vel, self.final_y_acc]
+        self.time_to_complete = self.path_time
+        (self.x_path, self.y_path, self.t_path) = path_generator.gen_path(x_start, y_start, self.x_stop, self.y_stop, self.time_to_complete)
+        self.path.set(xdata=self.x_path, ydata=self.y_path)
+        self.canvas.draw()
+
+    def send_path(self):
+        send_x = self.x_stop + [self.path_time]
+        send_y = self.y_stop + [self.path_time]
+        BP_Coms.send_path(send_x, send_y)
+
+    def on_click(self, event):
+        if event.inaxes is not None:
+            self.x = event.xdata
+            self.y = event.ydata
+            print(self.x, self.y)
+            self.final_pos.set(xdata = self.x, ydata = self.y)
+            self.canvas.draw()
+        else:
+            print('Clicked ouside axes bounds but inside plot window')
+        self.clicked = True
+
+    def on_release(self, event):
+        self.clicked = False
+
+    def on_move(self, event):
+        if (self.clicked):   
+            if event.inaxes is not None:
+                self.x = event.xdata
+                self.y = event.ydata
+                print(self.x, self.y)
+                self.final_pos.set(xdata = self.x, ydata = self.y)
+                self.canvas.draw()
+            else:
+                print('Clicked ouside axes bounds but inside plot window')
+
+
+# Matplotlib widget
+class MplWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)   # Inherit from QWidget
+        self.canvas = MplCanvas()                  # Create canvas object
+        self.vbl = QtWidgets.QVBoxLayout()         # Set box for plotting
+        self.vbl.addWidget(self.canvas)
+        self.setLayout(self.vbl)
