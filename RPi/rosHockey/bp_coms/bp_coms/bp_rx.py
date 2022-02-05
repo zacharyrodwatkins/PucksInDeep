472e0c0de2dfe16831c6daaea3a43022d7213cb0
fizzer <zackwatkins604@gmail.com>
2022-02-04 16:42:12 -0800

    working bluepill reciver bp_rx

--- RPi/rosHockey/bp_coms/bp_coms/bp_rx.py
+++ RPi/rosHockey/bp_coms/bp_coms/bp_rx.py
@@ -17,7 +17,7 @@ class bp_rx(Node):
     def __init__(self):
         super().__init__('bp_rx')
         self.mallet_publisher = self.create_publisher(MalletPos, 'MALLET', 10)
-        self.motor_publisher = self.create_publisher(MalletPos, 'MOTOR', 10)
+        self.motor_publisher = self.create_publisher(MotorStatus, 'MOTOR', 10)
         self.time = self.create_timer(TIMER_PERIOD, self.get_bp_data)
         self.ser = None
         for i in range(3):
@@ -33,19 +33,19 @@ class bp_rx(Node):
             
     def get_bp_data(self):
         if (self.ser.in_waiting == NUM_BYTES):
-            mallet_status = self.ser.read(NUM_FLOATS)
+            mallet_status = self.ser.read(NUM_BYTES)
             floats = struct.unpack('fffffff', mallet_status)
             x,y,vx,vy,m1,m2,t = floats
             motor_msg = MotorStatus()
             mallet_msg = MalletPos()
-            motor_msg.M1_effort = m1
-            motor_msg.M2_effort = m2
+            motor_msg.m1effort = m1
+            motor_msg.m2effort = m2
             motor_msg.time_on_path = t
             mallet_msg.x = x 
             mallet_msg.y = y 
-            mallet_msg.x_vel = x_vel
-            mallet_msg.y_vel = y_vel
-            mallet_msg.time_on_path =  time_on_path
+            mallet_msg.vx = vx
+            mallet_msg.vy = vy
+            mallet_msg.time_on_path =  t
             self.mallet_publisher.publish(mallet_msg)
             self.motor_publisher.publish(motor_msg)
     
@@ -54,7 +54,7 @@ class bp_rx(Node):
 def main(args=None):
     rclpy.init(args=args)
     rx = bp_rx()
-    rclpy.spin(Mrx)
+    rclpy.spin(rx)
 
     rx.destroy_node()
     rclpy.shutdown()
