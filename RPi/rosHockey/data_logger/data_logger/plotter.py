70ad388fc8e29038da4cb1a775e3ecabe1b38660
fizzer <zackwatkins604@gmail.com>
2022-02-15 14:35:46 -0800

    add plotter

---
+++ RPi/rosHockey/data_logger/data_logger/plotter.py
@@ -0,0 +1,65 @@
+from click import pass_obj
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import json
+from solve_for_coefs import *
+
+
+
+def plot_paths(log_file, n_paths = -1):
+    with open(log_file, "r") as f:
+        lines = f.readlines()
+
+    if n_paths >= 0:
+        pass
+        # only take most recent n paths
+        # new_pat_indicies = 
+        
+    paths = []
+    functions = []
+    # cols = {"x", 'y','vx','vy','ax','ay','time_on_path', 'logger_time', 'path_index'}
+    path_index = 0
+    # positions = pd.DataFrame(columns=cols)
+
+    dfs = []
+    for line in lines:
+        data = json.loads(line)
+        if 'MALLET' in data.keys():
+            data = data['MALLET']
+            data['path_index'] = path_index
+            # print(data)
+            new_df = pd.DataFrame(data,index = [0])
+            dfs.append(new_df)
+            # positions.append(data, ignore_index = True)
+            
+        elif 'PATH' in data.keys():
+            last_position_data = dfs[-1]
+            if path_index == 0:
+                dummy_path = {}
+                dummy_path['x'] = last_position_data['x']
+                dummy_path['y'] = last_position_data['y']
+                for key in ['vx','vy','ax','ay','t','logger_time']:
+                    dummy_path[key]=0
+
+                paths.append(pd.DataFrame(dummy_path))
+                functions.append((lambda t : dummy_path['x']), (lambda t : dummy_path['y']))
+
+            
+            path_index += 1
+            
+
+
+
+            paths.append(pd.DataFrame(data['PATH'], index=[0]))
+
+    positions = pd.concat(dfs).reset_index(drop=True)
+    paths = pd.concat(paths).reset_index(drop=True)
+    print(paths)
+
+if __name__ == "__main__":
+    data_file = '/home/fizzer/PucksInDeep/RPi/rosHockey/data_logger/data/2022-02-14 19:06:37.937629.csv'
+    plot_paths(data_file)
+
+
+    # paths.append({"x" : 0, 'y' : 0, ''})
