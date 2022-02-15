f6d9981e292b73baea9452ead7de9c942c51a170
fizzer <zackwatkins604@gmail.com>
2022-02-14 21:04:34 -0800

    Hard coded HLC

---
+++ RPi/rosHockey/HLC/HLC/main.py
@@ -0,0 +1,76 @@
+import time
+import rclpy
+from rclpy.node import Node
+from hockey_msgs.msg import PuckStatus, NextPath
+TIMER_PERIOD = 1/1000
+
+
+class HLC(Node):
+  
+    def __init__(self):
+        self.puck_x = 0.0
+        self.puck_y = 0.0
+        self.puck_vx = 0.0
+        self.puck_vy = 0.0
+
+        self.crossing_line = 20.0
+
+        self.mallet_x = 0.0
+        self.mallet_y = self.crossing_line
+        self.mallet_t = 0.0
+
+        super().__init__('HLC')
+        self.puck_status_subscription = self.create_subscription(PuckStatus,'PuckStatus',self.puck_callback,1)
+        self.path_publisher = self.create_publisher(NextPath, 'PATH', 10)
+        
+        self.puck_status_publisher = self.create_publisher(PuckStatus, 'PuckStatus', 10)
+
+    def update_path(self):
+        msg = NextPath()
+        msg.x = float(self.mallet_x)
+        msg.y = float(self.mallet_y)
+        msg.vx = 0.0
+        msg.vy = 0.0
+        msg.ax = 0.0
+        msg.ay = 0.0
+        msg.t = self.mallet_t
+        self.path_publisher.publish(msg)
+
+    def compute_crossing_point(self):
+        print(self.puck_vy)
+        if (self.puck_vy < -0.1):
+            if self.mallet_x< 0:
+                self.mallet_x = 0
+            if self.mallet_x> 75:
+                self.mallet_x = 75
+            self.mallet_t = (self.crossing_line-self.puck_y)/self.puck_vy
+            self.mallet_x = self.puck_x+self.puck_vx*self.mallet_t
+
+        else:
+            if self.mallet_x< 0:
+                self.mallet_x = 0
+            if self.mallet_x> 75:
+                self.mallet_x = 75
+            self.mallet_x = self.puck_x
+            self.mallet_t = 1.0
+
+
+    def puck_callback(self, msg):
+        self.puck_x = msg.x
+        self.puck_y = msg.y
+        self.puck_vx = msg.x_vel
+        self.puck_vy = msg.y_vel
+
+        self.compute_crossing_point()
+        self.update_path()
+
+def main(args=None):
+    rclpy.init(args=args)
+    hlc = HLC()
+    rclpy.spin(hlc)
+
+    hlc.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
