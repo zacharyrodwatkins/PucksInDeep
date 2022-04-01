fdca8ea0ec44f5541c413d591663c503b8e54cd8
fizzer <zackwatkins604@gmail.com>
2022-03-31 21:08:03 -0700

    need to switch branches

--- RPi/camera/run_camera.bash
+++ RPi/camera/run_camera.bash
@@ -1,1 +1,2 @@
-raspivid -o upd://10.42.0.1:5000 -w 640 -h 480 -t 0 --fps 90 
+killall raspivid &> /dev/null
+raspivid -o udp://10.42.0.1:5000 -w 640 -h 480 -t 0 -fps 90 -md 6
--- modeling/custom_layers.py
+++ modeling/custom_layers.py
@@ -7,19 +7,22 @@ import pickle as pkl
 
 class DitchLastArg(Layer):
 
-  def __init__(self, name=None, **kwargs):
-      super(DitchLastArg, self).__init__()
+    def __init__(self, name=None, **kwargs):
+        super(DitchLastArg, self).__init__()
 
-  def build(self, input_shape):  # Create the state of the layer (weights)
-    return
+    def build(self, input_shape):  # Create the state of the layer (weights)
+        return
 
-  def call(self, inputs):  # Defines the computation from inputs to outputs
-      return inputs[:-1]
+    def call(self, inputs):  # Defines the computation from inputs to outputs
+        return inputs[:-1]
 
-# x = np.array([1,2,3,4])
-# l = DitchLastArg()
+class AddArg(Layer):
 
-# y = l(x)
-# print(y)
+    def __init__(self, name=None, **kwargs):
+        super(DitchLastArg, self).__init__()
+
+    def call(self,inputs):
+        return tf.concat(inputs, tf.zeros.inputs.shape(-1), axis = -1)
+
 
 
