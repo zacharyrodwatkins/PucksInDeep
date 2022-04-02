a141a7b2fc3ec3c7bb3299443e7e10d6cccee14d
fizzer <zackwatkins604@gmail.com>
2022-04-02 13:47:18 -0700

    Added savgol code

---
+++ RPi/savgol/CMakeFiles/SavgolCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SavgolCpp.dir/lib/savgol.cpp.o"
+  "SavgolCpp"
+  "SavgolCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SavgolCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
