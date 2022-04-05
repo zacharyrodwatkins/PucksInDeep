0c50a25fd74a320f5e1ef5ca730ac2b5e210bbb2
pham <erpham16@gmail.com>
2022-04-05 11:01:21 -0700

    learning monitor

--- RPi/rosHockey/ID_tool/ID_tool/ModelLearningMonitor.py
+++ RPi/rosHockey/ID_tool/ID_tool/ModelLearningMonitor.py
@@ -1,113 +1,108 @@
+from cProfile import label
 from os import read
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
-import rclpy
-from rclpy.node import Node
-
-from hockey_msgs.msg import MotorStatus, NextPath
 
 
-class MotorSubscriber(Node):
+ser = serial.Serial('/dev/ttyUSB0',460800, timeout=1)
+ser.flush()
 
-    def __init__(self):
-        super().__init__('motor_subscriber')
-        self.subscription = self.create_subscription(
-            MotorStatus,
-            'MOTOR',
-            self.listener_callback,
-            10)
-        self.publisher_ = self.create_publisher(NextPath, 'PATH', 10)
-        self.V1 = []
-        self.V2 = []
-        self.t = []
-        self.path_time = 0.5
-        self.path_steps = 500
-        self.subscription  # prevent unused variable warning
+last = [0,0,0]
+last_V1 = []
+last_V2 = []
+V1 = []
+V2 = []
+er1 = []
+er2 = []
+t = []
+path_steps = 500
+path_time = 0.5
+done = False
+first = 0
+prev_t =None
+while True:
+    while not done:
+        line = ser.readline()
+        line = line.decode()
+        if not (len(line) == 0):
+            last = line.split(" ")
+            print(last)
+            last[-1] = last[-1].strip("\n")
+            last[-1] = last[-1].strip("\r")
+            last = [float(x) for x in last]
+            V1.append(last[0])
+            V2.append(last[1])
+            er1.append(last[2])
+            er2.append(last[3])
+            t.append(last[4])
+            # print(t[-1])
+            
+            if last[4] == path_steps-1:
+                done = True
 
-        
-    def publish(self):
-        msg = NextPath()
-        msg.x = 69.0
-        msg.y = 69.0
-        msg.vx = 69.0
-        msg.vy = 69.0
-        msg.ax = 69.0
-        msg.ay = 69.0
-        msg.t = 69.0
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.x)
-        
-    def listener_callback(self, msg):
-        path_time = 0.5
-        path_steps = 500
-        
-        self.V1.append(msg.m1effort)
-        self.V2.append(msg.m2effort)
-        self.t.append(msg.time_on_path)
-        print(len(self.V1))
-        if (len(self.V1) == path_steps):
-            print(self.V1)
-            print(self.V2)
+    
+    t = [time/1E3 for time in t]
 
-            for i in range(len(self.t)):
-                if self.t[i] == 1:
-                    self.t = self.t[-1*i:] + self.t[:-1*i]
-                    self.V1 = self.V1[-1*i:] + self.V1[:-1*i]
-                    self.V2 = self.V2[-1*i:] + self.V2[:-1*i]
+    fig, ax1 = plt.subplots()
 
-            # t = np.linspace(0, self.path_time, self.path_steps)
-            plt.scatter(self.t, self.V1)
-            plt.scatter(self.t, self.V2)
-            plt.legend(["m1effort", "m2effort"])
-            plt.show()
-            if not (input("send? (Y/n)") == "n"):
-                self.publish()
-            else:
-                print("should save V1/V2 here")
-            self.V1 = []
-            self.V2 = []
-            self.t = []
+    effort_x = np.array(V1)+np.array(V2)
+    effort_y = np.array(V1)-np.array(V2)
 
-        self.get_logger().info('M1:%s    M2:%s' % (msg.m1effort, msg.m2effort))
+    ax2 = ax1.twinx()
+    # ax1.plot(t, V1)
+    # ax1.plot(t, V2)
+
+    ax1.plot(t, effort_x, label='effort x')
+    ax1.plot(t,effort_y, label='effort y')
 
 
-def main(args=None):
-    rclpy.init(args=args)
+    V1_poly = np.polyfit(t, V1, 4)
+    V2_poly = np.polyfit(t, V2, 4)
+    
+    # ax1.plot(t, np.polyval(V1_poly, t))
+    # ax1.plot(t, np.polyval(V2_poly, t))
+    if (len(last_V1) > 0):
+        # try:
+        print(prev_t)
+        effort_x_last = np.array(last_V1)+np.array(last_V2)
+        effort_y_last= np.array(last_V1)-np.array(last_V2)
 
-    minimal_subscriber = MotorSubscriber()
+        # ax1.plot(t, last_V1)
+        # ax1.plot(t, last_V2)
+        ax1.plot(prev_t, effort_x_last, label='previous x effort')
+        ax1.plot(prev_t, effort_y_last, label='previous y effort')
+        # except:
+        #     print("missed smthn")
+    # ax2.plot(t, er1, "black")
+    # ax2.plot(t, er2, "gray")
 
-    rclpy.spin(minimal_subscriber)
+    # err_x = np.array(er1)+np.array(er2)
+    # err_y = np.array(er1)-np.array(er2)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
+    ax2.plot(t, er1, "black",label = 'x error')
+    ax2.plot(t, er2, "gray", label = 'y error')
 
+    # ax1.legend(["next V1", "next V2", "V1 last", "V2 last"])
+    ax1.legend()
+    # ax2.legend(["theta 1 error", "theta 2 error"])
+    ax2.legend()
+    plt.show()
+    
+    print("V1 Poly: {}".format(V1_poly))
+    print("V2 Poly: {}".format(V2_poly))
 
-if __name__ == '__main__':
-    main()
+    if not (input("RUN PATH? (Y/n)") == "n"):
+        ser.write("a".encode())
+        done = False
 
-
-# ser = serial.Serial('/dev/ttyUSB0',460800, timeout=1)
-
-# path_time = 0.5
-# path_steps = 500
-# V1 = []
-# V2 = []
-# for i in range(path_steps*2):
-#     if i < 500:
-#         V1.append(ser.readline().rstrip()[2:-1])
-#     else:
-#         V2.append(ser.readline().rstrip()[2:-1])
-
-# print(V1)
-# print(V2)
-
-# t = np.linspace(0, path_time, path_steps)
-# plt.plot(t, V1)
-# plt.plot(t, V2)
-# plt.yticks(list(np.linspace(0,127,12)))
-# plt.legend(["V1", "V2"])
-# plt.show()
+    prev_t = True
+    if prev_t is not None:
+        prev_t = t
+    last_V1 = V1
+    last_V2 = V2
+    V1 = []
+    V2 = []
+    er1 = []
+    er2 = []
+    t = []
